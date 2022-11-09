64ad2e438ba4713fd9a72cb8c4bb67dadad75e32
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-11-10 00:05:14 +0100

    added calendar

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+import "../style/calendarcss.css";
+
 
 const Homepage = () => {
   return (
     <div>
         <h1>Homepage</h1>
+        <Calendar />
     </div>
+
     );
 }
 

9b93a42807ff91da1477802be74240a80bb254b7
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-12-03 15:10:59 +0100

    added admin page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import './App.css';
-import React  from "react";
+import "./App.css";
+import React from "react";
 import "./style/homestyle.css";
 import Logout from "./components/logout";
-import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import Homepage from "./Pages/home";
 import RequireAuth from "./app/RequireAuth";
 import Loading from "./components/loading";
 import NotFoundPage from "./Pages/404page/404";
+import AdminPage from "./Pages/adminPage";
 
 function App() {
-
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<RequireAuth/>} >
-                <Route path="/" element={<Homepage/>}/>
-            </Route>
-            <Route path="*" element={<NotFoundPage/>}/>
-            <Route path="/logout" element={<Logout/>}/>
-            <Route path="/loading" element={<Loading/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<RequireAuth />}>
+          <Route path="/" element={<Homepage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
     </BrowserRouter>
-
-
   );
 }
 
---
+++ src/Pages/adminPage.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AdminPage = () => {
+    return (
+        <div>
+            <h1>Admin Page</h1>
+            <p>Only Admins can see this page</p>
+        </div>
+    )
+}
+
+export default AdminPage;

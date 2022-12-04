98e982523a0604dec089602b2b3b55e374c0b93f
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-12-04 11:09:49 +0100

    Update adminPage.js

--- src/Pages/adminPage.js
+++ src/Pages/adminPage.js
@@ -1,12 +1,29 @@
-import React from "react"
+import React from "react";
+import axios from "axios";
+import ScvLogo from "../style/school_logo.png";
+import { store } from "../app/store";
+import { setUserLoading, userLogin } from "../auth/userSlice";
+import { useDispatch } from "react-redux";
 
 const AdminPage = () => {
-    return (
-        <div>
-            <h1>Admin Page</h1>
-            <p>Only Admins can see this page</p>
-        </div>
-    )
-}
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const odgovor = await axios.get("http://localhost:5050/api/user", {
+      withCredentials: true,
+    });
+    if (odgovor.data.isadmin === true) {
+      alert("Da");
+    } else {
+      alert("You are not an admin!");
+    }
+  }
+  return (
+    <div>
+      <h1>Admin Page</h1>
+      <p>Only Admins can see this page</p>
+    </div>
+  );
+};
 
 export default AdminPage;

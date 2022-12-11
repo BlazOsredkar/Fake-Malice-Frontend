2a64b25b6f3914d3dbbcfc688f163dc2d9357550
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-12-11 23:52:47 +0100

    added ugly css

--- src/Pages/forgotPassword.js
+++ src/Pages/forgotPassword.js
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
 import axios from "axios";
-import {store} from "../app/store";
-import {setUserLoading, userLogin} from "../auth/userSlice";
+import ScvLogo from "../style/school_logo.png";
 
 const ForgotPassword = () => {
     //check if mail exists in database
+    //{emailExists ? <p>Na vaš email smo poslali povezavo za ponastavitev gesla.</p> : <p>Email ne obstaja.</p>}
     const [eposta, setEmail] = useState("");
     const [emailExists, setEmailExists] = useState(false);
 
@@ -18,10 +18,12 @@ const ForgotPassword = () => {
             const response = await axios.post('http://localhost:5050/api/forgotPassword', {eposta}, {withCredentials:true})
             if(response.status === 200){
                 setEmailExists(true);
+                alert("Na vaš email smo poslali povezavo za ponastavitev gesla.");
             }
         }
         catch (e) {
             setEmailExists(false);
+            alert("Uporabnik s tem emailom ne obstaja.");
         }
 
     }
@@ -29,17 +31,22 @@ const ForgotPassword = () => {
 
     return(
 
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="email" className="inputEmail input" name="eposta" placeholder="Email" required/>
-                <input type="submit" value="Pošlji"/>
-            </form>
-            {emailExists ? <p>Na vaš email smo poslali povezavo za ponastavitev gesla.</p> : <p>Email ne obstaja.</p>}
+        <>
+            <div className="App">
+                <img src={ScvLogo} alt="logo" className="logo"/>
+                <form onSubmit={handleSubmit}>
+                    <input type="email" className="inputEmail input" name="eposta" placeholder="Email" required/>
+                    <input type="submit" className="submit" value="Pošlji"/>
+                </form>
 
-        </div>
 
 
-    )
+            </div>
+
+        </>
+
+
+    );
 
 
 }
